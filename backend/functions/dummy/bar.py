82b9fd036b1cf8c344532e657a884f38024a3ba7
longwind48 <longwind48@gmail.com>
2024-09-22 14:32:43 +0800

    doc: fix wrong url for local testing

--- backend/functions/dummy/bar.py
+++ backend/functions/dummy/bar.py
@@ -2,7 +2,7 @@
 This is a dummy file for testing.
 
 url for local testing:
-http://127.0.0.1:5001/schemessg-v3-dev/us-central1/bar
+http://127.0.0.1:5001/schemessg-v3-dev/asia-southeast1/bar
 """
 
 from firebase_functions import https_fn
--- backend/functions/dummy/foo.py
+++ backend/functions/dummy/foo.py
@@ -2,7 +2,7 @@
 This is a dummy file for testing.
 
 url for local testing:
-http://127.0.0.1:5001/schemessg-v3-dev/us-central1/foo
+http://127.0.0.1:5001/schemessg-v3-dev/asia-southeast1/foo
 """
 
 from firebase_functions import https_fn
