498d0c16840dbf83c688eca500208b20403189b5
chunxtan <chunxiaotan99@gmail.com>
2024-09-28 09:01:57 +0800

    #41 tweak font styling

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,14 +3,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../assets/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  weight: "100 300 500 900",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ import React from 'react';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} antialiased`}>
         <NextUIProvider>
           <MainHeader/>
           {children}
