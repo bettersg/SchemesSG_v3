729a83646e61ff8369771cf15f23979500a66e04
longwind48 <longwind48@gmail.com>
2024-12-02 23:16:24 +0800

    chore: rm unncessary files

--- frontend_old/static/js/schemescase.js
+++
@@ -1,9 +0,0 @@
-const scriptURLschemescase = 'https://script.google.com/macros/s/AKfycbyqGHt2p224ebUahB6XDOgxtru9fvXm3YonCKcPus--p8e57TFB/exec'
-const schemescase = document.getElementById("schemescase")
-schemescase.addEventListener('submit', e => {
-    e.preventDefault()
-    $("#schemescase-submit").html("Submitting...");
-    fetch(scriptURLschemescase, { method: 'POST', body: new FormData(schemescase) })
-        .then(response => window.location.replace("gotit.html"))
-        .catch(error => console.error('Error!', error.message))
-})
