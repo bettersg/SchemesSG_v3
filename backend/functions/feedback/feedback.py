6ac90a091b2600401671857f9fd680648f12cdc8
Elroy Chua <elroymx@gmail.com>
2024-12-11 11:15:41 +0800

    quick fix to timestamp in req data

--- backend/functions/feedback/feedback.py
+++ backend/functions/feedback/feedback.py
@@ -52,7 +52,7 @@ def feedback(req: https_fn.Request) -> https_fn.Response:
         feedback_text = request_json.get("feedbackText")
         userName = request_json.get("userName")
         userEmail = request_json.get("userEmail")
-        timestamp = datetime.datetime.now(timezone.utc)
+        timestamp = datetime.now(timezone.utc)
 
         if not feedback_text or not timestamp:
             return https_fn.Response(
