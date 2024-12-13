31e274121a89e359a4c1ca14681fb848bf6bfc54
alyssaongyx <ongyixian@gmail.com>
2024-12-13 22:40:44 +0800

    create query ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,47 @@
-'use client';
+"use client";
 
 import MainChat from "@/components/main-chat/main-chat";
-import SchemesList from "@/components/schemes/schemes-list"
-import React, { useState } from 'react';
+import SchemesList from "@/components/schemes/schemes-list";
+import React, { useState } from "react";
 import SearchBar from "@/components/search-bar/search-bar";
 import { useChat } from "./providers";
-import classes from "../components/main-layout/main-layout.module.css"
+import classes from "../components/main-layout/main-layout.module.css";
+import QueryGenerator from "@/components/query-generator/searchbar";
 
 export default function Home() {
-    const { schemes } = useChat();
-    const [sessionId, setSessionId] = useState<string>("");
+  const { schemes } = useChat();
+  const [sessionId, setSessionId] = useState<string>("");
 
-    return (
-        <main className={classes.homePage}>
-            {
-                schemes.length > 0
-                ? <div style={{ display:"grid", gridTemplateColumns: "2fr 3fr", gap: "0.5rem"}}>
-                    <MainChat sessionId={sessionId}/>
-                    <SchemesList schemes={schemes} />
-                </div>
-                :
-                <div>
-                    <div className={classes.welcomeMsg}>
-                        <div className="font-extrabold text-2xl" style={{ display:"flex", justifyContent: "center" }}>
-                            <p style={{ color:"#171347" }}>Welcome to Schemes </p>
-                            <p style={{ color:"#008AFF" }}>SG</p>
-                        </div>
-                        <p className="font-medium text-center" style={{ color:"#171347" }}>This is an AI-supported search engine for public social assistance schemes in Singapore.</p>
-                    </div>
-                    <SearchBar setSessionId={setSessionId} />
-                </div>
-            }
-        </main>
-    )
+  return (
+    <main className={classes.homePage}>
+      {schemes.length > 0 ? (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "2fr 3fr",
+            gap: "0.5rem",
+          }}
+        >
+          <MainChat sessionId={sessionId} />
+          <SchemesList schemes={schemes} />
+        </div>
+      ) : (
+        <div className={classes.welcomeMsg}>
+          <div
+            className="font-extrabold text-2xl"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <p style={{ color: "#171347" }}>Welcome to Schemes </p>
+            <p style={{ color: "#008AFF" }}>SG</p>
+          </div>
+          <p className="font-medium text-center" style={{ color: "#171347" }}>
+            This is an AI-supported search engine for public social assistance
+            schemes in Singapore.
+          </p>
+          <QueryGenerator />
+          <SearchBar setSessionId={setSessionId} />
+        </div>
+      )}
+    </main>
+  );
 }
