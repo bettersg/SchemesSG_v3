a9936a487ae53950df22975ac9860545a054575a
alyssaongyx <ongyixian@gmail.com>
2024-12-14 17:23:41 +0800

    add back original logic

--- frontend/src/components/search-bar/search-bar.tsx
+++ frontend/src/components/search-bar/search-bar.tsx
@@ -5,6 +5,7 @@ import { Button, Spinner, Textarea } from "@nextui-org/react";
 import { SearchIcon } from "../../assets/icons/search-icon";
 import classes from "./search-bar.module.css";
 import { useChat } from "@/app/providers";
+import { Scheme } from "../schemes/schemes-list";
 
 interface SearchBarProps {
   setSessionId: (val: string) => void;
@@ -59,6 +60,71 @@ export default function SearchBar({
     setUserInput("");
   };
 
+  const mapToScheme = (rawData: any): Scheme => {
+    return {
+      schemeType: rawData["Scheme Type"] || "",
+      schemeName: rawData["Scheme"] || "",
+      targetAudience: rawData["Who's it for"] || "",
+      agency: rawData["Agency"] || "",
+      description: rawData["Description"] || "",
+      scrapedText: rawData["scraped_text"] || "",
+      benefits: rawData["What it gives"] || "",
+      link: rawData["Link"] || "",
+      image: rawData["Image"] || "",
+      searchBooster: rawData["search_booster(WL)"] || "",
+      schemeId: rawData["scheme_id"] || "",
+      query: rawData["query"] || "",
+      similarity: rawData["Similarity"] || 0,
+      quintile: rawData["Quintile"] || 0
+    };
+  };
+
+  const getSchemes = async () => {
+    const url = "http://localhost:5001/schemessg-v3-dev/asia-southeast1/schemes_search";
+
+    const requestBody = {
+      query: userInput,
+      top_k: 20,
+      similarity_threshold: 0
+    };
+
+    try {
+      setIsBotResponseGenerating(true);
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(requestBody)
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const res = await response.json();
+      const sessionId: string = res["sessionID"];
+      setIsBotResponseGenerating(false);
+      const schemesRes: Scheme[] = res.data.map(mapToScheme);
+      return { schemesRes, sessionId };
+    } catch (error) {
+      console.error("Error making POST request:", error);
+      setIsBotResponseGenerating(false);
+      return { schemesRes: [], sessionId: "" };
+    }
+  };
+
+  const handleSend = async () => {
+    if (userInput.trim()) {
+      const { schemesRes, sessionId } = await getSchemes();
+      if (schemesRes.length > 0 && sessionId !== "") {
+        schemesRes && setSchemes(schemesRes);
+        setSessionId(sessionId);
+        handleUserInput(userInput);
+      }
+    }
+  };
+
   return (
     <>
       <Textarea
@@ -67,7 +133,7 @@ export default function SearchBar({
         onKeyDown={async (e) => {
           if (e.key === "Enter" && !isBotResponseGenerating) {
             e.preventDefault();
-            await handleUserInput(userInput);
+            await handleSend();
           }
         }}
         className={classes.searchBar}
@@ -88,7 +154,7 @@ export default function SearchBar({
               isIconOnly
               size="sm"
               radius="full"
-              onClick={async () => await handleUserInput(userInput)}
+              onClick={async () => await handleSend()}
             >
               <SearchIcon />
             </Button>
