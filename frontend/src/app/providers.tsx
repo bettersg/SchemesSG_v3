0b0f59bedc685e142bda03232c48e800f8368c2d
Elroy Chua <elroymx@gmail.com>
2024-12-14 09:55:28 +0800

    Hotfix providers localStorage is not defined error

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,19 +1,25 @@
-'use client';
+"use client";
 
-import { Scheme } from '@/components/schemes/schemes-list';
-import { NextUIProvider } from '@nextui-org/react';
-import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import { Scheme } from "@/components/schemes/schemes-list";
+import { NextUIProvider } from "@nextui-org/react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 // Chat Context
 export type Message = {
-  type: 'user' | 'bot';
+  type: "user" | "bot";
   text: string;
 };
 
 type ChatContextType = {
-  userQuery: string,
-  setUserQuery: React.Dispatch<React.SetStateAction<string>>,
-  messages: Message[],
+  userQuery: string;
+  setUserQuery: React.Dispatch<React.SetStateAction<string>>;
+  messages: Message[];
   setMessages: React.Dispatch<React.SetStateAction<Message[]>>;
   schemes: Scheme[];
   setSchemes: React.Dispatch<React.SetStateAction<Scheme[]>>;
@@ -24,16 +30,30 @@ const ChatContext = createContext<ChatContextType | undefined>(undefined);
 export const ChatProvider = ({ children }: { children: ReactNode }) => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [userQuery, setUserQuery] = useState<string>("");
-  const [schemes, setSchemes] = useState<Scheme[]>(
-    () => JSON.parse(localStorage.getItem('schemes') || '[]')
-  );
+  const [schemes, setSchemes] = useState<Scheme[]>([]);
 
   useEffect(() => {
-    localStorage.setItem('schemes', JSON.stringify(schemes));
+    const savedSchemes = localStorage.getItem("schemes");
+    if (savedSchemes) {
+      setSchemes(JSON.parse(savedSchemes));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("schemes", JSON.stringify(schemes));
   }, [schemes]);
 
   return (
-    <ChatContext.Provider value={{ messages, setMessages, userQuery, setUserQuery, schemes, setSchemes }}>
+    <ChatContext.Provider
+      value={{
+        messages,
+        setMessages,
+        userQuery,
+        setUserQuery,
+        schemes,
+        setSchemes,
+      }}
+    >
       {children}
     </ChatContext.Provider>
   );
@@ -42,7 +62,7 @@ export const ChatProvider = ({ children }: { children: ReactNode }) => {
 export const useChat = () => {
   const context = useContext(ChatContext);
   if (!context) {
-    throw new Error('useChat must be used within a ChatProvider');
+    throw new Error("useChat must be used within a ChatProvider");
   }
   return context;
 };
