aec731655004d0c50dea521162b381a2d32456ca
Elroy Chua <elroymx@gmail.com>
2024-12-14 08:09:16 +0800

    Add responsiveness to homePage title, subtitle and searchbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,36 +1,56 @@
-'use client';
+"use client";
 
 import MainChat from "@/components/main-chat/main-chat";
-import SchemesList from "@/components/schemes/schemes-list"
-import React, { useState } from 'react';
+import SchemesList from "@/components/schemes/schemes-list";
 import SearchBar from "@/components/search-bar/search-bar";
+import { useState } from "react";
+import classes from "../components/main-layout/main-layout.module.css";
 import { useChat } from "./providers";
-import classes from "../components/main-layout/main-layout.module.css"
 
 export default function Home() {
-    const { schemes } = useChat();
-    const [sessionId, setSessionId] = useState<string>("");
+  const { schemes } = useChat();
+  const [sessionId, setSessionId] = useState<string>("");
 
-    return (
-        <main className={classes.homePage}>
-            {
-                schemes.length > 0
-                ? <div style={{ display:"grid", gridTemplateColumns: "2fr 3fr", gap: "0.5rem"}}>
-                    <MainChat sessionId={sessionId}/>
-                    <SchemesList schemes={schemes} />
+  return (
+    <main className={classes.homePage}>
+      {schemes.length > 0 ? (
+        <div className={classes.mainLayout}>
+          <MainChat sessionId={sessionId} />
+          <SchemesList schemes={schemes} />
+        </div>
+      ) : (
+        <div>
+          <div className={classes.welcomeMsg}>
+            {/* Desktop*/}
+            <div className="hidden md:block">
+              <h1 className="text-center text-4xl font-bold">
+                <span className="text-[#171347]">Welcome to Schemes</span>
+                <span className="text-[#008AFF]">SG</span>
+              </h1>
+              <p className="text-[#171347] text-center mt-6 text-lg">
+                This is an AI-supported search engine for public social
+                assistance schemes in Singapore.
+              </p>
+            </div>
+
+            {/* Mobile*/}
+            <div className="block md:hidden">
+              <h1 className="text-[32px] font-bold leading-tight">
+                <div className="text-[#171347] text-center">Welcome to</div>
+                <div className="flex items-center justify-center gap-2">
+                  <span className="text-[#171347]">Schemes</span>
+                  <span className="text-[#008AFF]">SG</span>
                 </div>
-                :
-                <div>
-                    <div className={classes.welcomeMsg}>
-                        <div className="font-extrabold text-2xl" style={{ display:"flex", justifyContent: "center" }}>
-                            <p style={{ color:"#171347" }}>Welcome to Schemes </p>
-                            <p style={{ color:"#008AFF" }}>SG</p>
-                        </div>
-                        <p className="font-medium text-center" style={{ color:"#171347" }}>This is an AI-supported search engine for public social assistance schemes in Singapore.</p>
-                    </div>
-                    <SearchBar setSessionId={setSessionId} />
-                </div>
-            }
-        </main>
-    )
+              </h1>
+              <p className="text-[#171347] mt-4 text-center leading-snug text-base">
+                This is an AI-supported search engine for public social
+                assistance schemes in Singapore.
+              </p>
+            </div>
+          </div>
+          <SearchBar setSessionId={setSessionId} />
+        </div>
+      )}
+    </main>
+  );
 }
