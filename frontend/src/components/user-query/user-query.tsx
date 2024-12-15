7ba0bf1a602761078184e927e821dc076f7f8f0c
Elroy Chua <elroymx@gmail.com>
2024-12-15 22:36:43 +0800

    fix build errors

--- frontend/src/components/user-query/user-query.tsx
+++ frontend/src/components/user-query/user-query.tsx
@@ -1,38 +1,50 @@
-import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
-import classes from './user-query.module.css';
 import { useChat } from "@/app/providers";
+import { ResetIcon } from "@/assets/icons/reset-icon";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import { Button, Tooltip, useDisclosure } from "@nextui-org/react";
 import ResetQueryModal from "../reset-query-modal/reset-query-modal";
-import { ResetIcon } from "@/assets/icons/reset-icon";
+import classes from "./user-query.module.css";
 
 export default function UserQuery() {
-    const { userQuery, setSchemes } = useChat();
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { userQuery, setSchemes } = useChat();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-    const handleReset = () => {
-      localStorage.removeItem('schemes');
-      setSchemes([]);
-    };
+  const handleReset = () => {
+    localStorage.removeItem("schemes");
+    setSchemes([]);
+  };
 
-    return (
-        <Card
-            className={`${classes.card} ${classes.userQuery}`}
-            fullWidth={false}
-            radius="lg"
-            shadow="none"
-        >
-            <CardHeader className={`justify-between ${classes.cardHeader}`}>
-                <h4 className="text-small leading-none text-default-600" >Your query is:</h4>
-                <Tooltip content="Reset Query" offset={-7}>
-                    <Button onPress={onOpen} isIconOnly radius="full" size="sm" className={`${classes.resetButton}`}>
-                        <ResetIcon />
-                    </Button>
-                </Tooltip>
-                <ResetQueryModal isOpen={isOpen} onOpenChange={onOpenChange} handleReset={handleReset} />
-            </CardHeader>
-            <CardBody className={`${classes.cardBody}`}>
-                <b>{userQuery}</b>
-            </CardBody>
-        </Card>
-    )
+  return (
+    <Card
+      className={`${classes.card} ${classes.userQuery}`}
+      fullWidth={false}
+      radius="lg"
+      shadow="none"
+    >
+      <CardHeader className={`justify-between ${classes.cardHeader}`}>
+        <h4 className="text-small leading-none text-default-600">
+          Your query is:
+        </h4>
+        <Tooltip content="Reset Query" offset={-7}>
+          <Button
+            onPress={onOpen}
+            isIconOnly
+            radius="full"
+            size="sm"
+            className={`${classes.resetButton}`}
+          >
+            <ResetIcon />
+          </Button>
+        </Tooltip>
+        <ResetQueryModal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          handleReset={handleReset}
+        />
+      </CardHeader>
+      <CardBody className={`${classes.cardBody}`}>
+        <b>{userQuery}</b>
+      </CardBody>
+    </Card>
+  );
 }
