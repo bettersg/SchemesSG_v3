22337c22edd1afd96aed332be5f9d3b5cc139641
Elroy Chua <elroymx@gmail.com>
2024-12-16 17:37:14 +0800

    add viewport instructions to disable zooming

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import classes from "../components/main-layout/main-layout.module.css";
-import MainHeader from '@/components/main-header/main-header';
-import { NextUIProvider } from '@nextui-org/system';
-import React from 'react';
 import MainFooter from "@/components/main-footer/main-footer";
+import MainHeader from "@/components/main-header/main-header";
+import { NextUIProvider } from "@nextui-org/system";
+import type { Metadata, Viewport } from "next";
+import localFont from "next/font/local";
+import React from "react";
+import classes from "../components/main-layout/main-layout.module.css";
+import "./globals.css";
 import { ChatProvider } from "./providers";
 
 const geistSans = localFont({
@@ -14,25 +14,37 @@ const geistSans = localFont({
   weight: "100 200 300 400 500 600 700 800 900",
 });
 
-export const metadata: Metadata = {
-  title: "Schemes SG",
-  description: "One stop directory and AI-enabled search to help make sense of assistance schemes in Singapore.",
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
+  viewportFit: "cover",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: "Schemes SG",
+  description:
+    "One stop directory and AI-enabled search to help make sense of assistance schemes in Singapore.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={`${geistSans.variable} antialiased`}>
         <NextUIProvider>
           <ChatProvider>
-            <MainHeader/>
-            <div className={classes.contentWrapper}>
-              {children}
-            </div>
+            <MainHeader />
+            <div className={classes.contentWrapper}>{children}</div>
             <MainFooter />
           </ChatProvider>
         </NextUIProvider>
       </body>
     </html>
-  )
+  );
 }
