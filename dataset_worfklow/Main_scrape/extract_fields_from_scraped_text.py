50c7a2baa2f4dee7780f9fbaefa54b736fbd9583
James CHAN <jameschan@Jamess-MacBook-Pro-3.local>
2025-04-07 23:34:43 +0800

    add new fields to be extracted

---
+++ dataset_worfklow/Main_scrape/extract_fields_from_scraped_text.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import os
+from dotenv import dotenv_values, load_dotenv
+from langchain_openai import AzureChatOpenAI
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class Config:
+    def __init__(self):
+        load_dotenv("backend/functions/.env")
+
+        for key, value in dotenv_values().items():
+            setattr(self, key.lower(), value)
+
+    def __getattr__(self, item):
+        attr = os.getenv(item.upper())
+        if attr:
+            setattr(self, item.lower(), attr)
+        return attr
+
+
+class SchemesStructuredOutput(BaseModel):
+    """Extract information from the description of the scheme."""
+
+    address: Optional[str] = Field(
+        default=None, description="Location in the description"
+    )
+    phone: Optional[str] = Field(
+        default=None, description="Phone number in the description"
+    )
+    email: Optional[str] = Field(
+        default=None, description="Relevant email in the description"
+    )
+    purpose_of_scheme: Optional[str] = Field(
+        default=None, description="Purpose of the scheme in the description"
+    )
+    website: Optional[str] = Field(
+        default=None, description="Website in the description"
+    )
+    summary: Optional[str] = Field(
+        default=None, description="Concise summary of the text"
+    )
+    scheme_category: Optional[str] = Field(
+        default=None, description="Category of the scheme in the description"
+    )
+    target_beneficiaries: Optional[str] = Field(
+        default=None, description="Target beneficiaries of the scheme in the description"
+    )
+    eligibility_criteria: Optional[str] = Field(
+        default=None, description="Eligibility criteria of the scheme in the description"
+    )
+    implementation_agency: Optional[str] = Field(
+        default=None, description="Implementation agency of the scheme in the description"
+    )
+    eligibility_documents: Optional[str] = Field(
+        default=None, description="Eligibility documents of the scheme in the description"
+    )
+    application_process: Optional[str] = Field(
+        default=None, description="Application process of the scheme in the description"
+    )
+
+
+class TextExtract:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(TextExtract, cls).__new__(cls)
+            cls._instance._initialize_app()
+
+        return cls._instance
+
+    def init_chatbot(self):
+        config = Config()
+        return AzureChatOpenAI(
+            deployment_name=config.deployment,
+            azure_endpoint=config.endpoint,
+            openai_api_version=config.version,
+            openai_api_key=config.apikey,
+            openai_api_type=config.type,
+            model_name=config.model,
+            temperature=0,
+        )
+
+    def _initialize_app(self):
+        self.open_ai_client = self.init_chatbot()
+
+    def extract_text(self, text: str):
+        structured_llm = self.open_ai_client.with_structured_output(
+            SchemesStructuredOutput
+        )
+        return structured_llm.invoke(text)
+
+
+# df = pd.read_csv("backend/functions/ml_logic/schemes-updated-with-text.csv")
+# text_extract = TextExtract()
+# for index, row in df.iterrows():
+#     print(row["Scraped Text"])
+#     print(text_extract.extract_text(row["Scraped Text"]))
+#     breakpoint()
