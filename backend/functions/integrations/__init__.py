d45aa4ad0fade135a7c066d02e1935340f10b344
veeviann <veeviann96@gmail.com>
2026-05-26 17:20:58 +0800

    feat: added integrations for easier model management, requires updated .env file.

---
+++ backend/functions/integrations/__init__.py
@@ -0,0 +1,5 @@
+from .embeddings_manager import EmbeddingsManager
+from .firebase_manager import FirebaseManager
+from .llm_manager import LLMManager
+
+__all__ = ["LLMManager", "EmbeddingsManager", "FirebaseManager"]
